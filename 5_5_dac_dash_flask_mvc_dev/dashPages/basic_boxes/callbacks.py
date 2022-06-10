9be710724529ed82a473130763df273f380ef3bc
sixx chung <sixx.chung@gmail.com>
2022-06-10 17:27:50 +0900

    bbb

---
+++ 5_5_dac_dash_flask_mvc_dev/dashPages/basic_boxes/callbacks.py
@@ -0,0 +1,12 @@
+from app_dash import dash_app
+from dash.dependencies      import Input, Output, State
+
+from components.example_plots     import plot_scatter
+
+# Update figure on slider change
+@dash_app.callback(
+    Output('box-graph', 'figure'),
+    [Input('controlbar-slider', 'value')] )
+def update_box_graph(value):
+    # go.Figure()
+    return plot_scatter(value)
