64d166b046eea463cb64a5760f79d6b18317af9f
sixx chung <sixx.chung@gmail.com>
2022-06-13 01:31:38 +0900

    aa

--- 5_7_dac_dash_flask_mvc_dev_global/dashPages/basic_boxes/test.py
+++ 5_7_dac_dash_flask_mvc_dev_global/dashPages/basic_boxes/test.py
@@ -55,36 +55,37 @@ app.layout = dac.Page([
     ),
     dac.Body(
         dac.TabItems([
-            dac.TabItem(id='content_basic_boxes',
-                        children=html.Div(
-                            [
-                                html.H1("dddddd"),
-                                dac.SimpleBox(
-                                    style={'height': "600px"},
-                                    title="Box 1",
-                                    children=[
-                                        dcc.Graph(
-                                            id='box-graph',
-                                            config=dict(displayModeBar=False),
-                                            style={'width': '38vw'}
-                                        )
-                                    ]
-                                ),
-                                dac.SimpleBox(
-                                    style={'height': "600px"},
-                                    title="Box 2",
-                                    children=[
-                                        dcc.Graph(
-                                            figure=plot_scatter(),
-                                            config=dict(displayModeBar=False),
-                                            style={'width': '38vw'}
-                                        )
-                                    ]
-                                )
-                            ],
-                            className='row'
-                        )
-                        )
+            dac.TabItem(
+                # id='content_basic_boxes',
+                children=html.Div(
+                    # [
+                    html.H1("dddddd"),
+                    #     dac.SimpleBox(
+                    #         style={'height': "600px"},
+                    #         title="Box 1",
+                    #         children=[
+                    #             dcc.Graph(
+                    #                 id='box-graph',
+                    #                 config=dict(displayModeBar=False),
+                    #                 style={'width': '38vw'}
+                    #             )
+                    #         ]
+                    #     ),
+                    #     dac.SimpleBox(
+                    #         style={'height': "600px"},
+                    #         title="Box 2",
+                    #         children=[
+                    #             dcc.Graph(
+                    #                 figure=plot_scatter(),
+                    #                 config=dict(displayModeBar=False),
+                    #                 style={'width': '38vw'}
+                    #             )
+                    #         ]
+                    #     )
+                    # ],
+                    # className='row'
+                )
+            )
         ])
     ),
     dac.Controlbar(),
